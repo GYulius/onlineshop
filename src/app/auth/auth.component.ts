6e9cb1a828a034b4243a7af7b425d23daa5fb273
GYulius <giulianbqc@gmail.com>
2024-10-07 19:18:11 +0300

    Toggle Register-Login squashed

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -7,13 +7,14 @@ import {
   MatCardSubtitle,
   MatCardTitle
 } from "@angular/material/card";
-import {NgForOf} from "@angular/common";
+import {NgForOf, NgIf} from "@angular/common";
 import {FormsModule} from "@angular/forms";
 import {MatButton, MatIconButton} from "@angular/material/button";
 import {MatFormField, MatLabel, MatSuffix} from "@angular/material/form-field";
 import {MatIcon} from "@angular/material/icon";
 import {MatInput} from "@angular/material/input";
 import {AuthService} from "../services/auth.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-auth',
@@ -33,18 +34,22 @@ import {AuthService} from "../services/auth.service";
     MatIconButton,
     MatInput,
     MatLabel,
-    MatSuffix
+    MatSuffix,
+    NgIf
   ],
   templateUrl: './auth.component.html',
   styleUrl: './auth.component.css'
 })
 export class AuthComponent {
+
+  authType: string = "login";
+
   username: string = "";
   email: string = "";
   password: string = "";
   confirmPassword: string = "";
 
-  constructor(private authService: AuthService) {
+  constructor(private authService: AuthService, private router: Router) {
   }
 
   submitRegisterForm() {
@@ -69,6 +74,19 @@ export class AuthComponent {
 
     this.authService.login(payload).subscribe((response: any) => {
       console.log(response);
+      if(response.status == 200) {
+        // daca aceasta conditie este adevarata inseamna ca ne-am logat cu succes
+
+        this.router.navigateByUrl("/dashboard");
+      }
+    },(errorResponse) => {
+      console.log(errorResponse);
+      console.log(errorResponse.error);
+      alert(errorResponse.error.message);
     });
+  }
+
+  onAuthTypeChange(authType: string){
+    this.authType = authType;
   }
 }
